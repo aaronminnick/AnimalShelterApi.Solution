99ba0fa3bc635e18e1a6318c5ea3c30aaa35e3c3
Aaron Minnick <abminnick@gmail.com>
2022-01-21 11:48:57 -0800

    clean up code

--- AnimalShelterApi/Models/AnimalShelterContext.cs
+++ AnimalShelterApi/Models/AnimalShelterContext.cs
@@ -5,13 +5,7 @@ namespace AnimalShelterApi.Models
   public class AnimalShelterContext : DbContext
   {
     public DbSet<Animal> Animals {get; set;}
-    // public DbSet<ClassClass> ClassClass {get; set;}
 
     public AnimalShelterContext(DbContextOptions options) : base(options) {}
-
-    // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    // {
-    //   optionsBuilder.UseLazyLoadingProxies();
-    // }
   }
 }
