cfdc3c43486d84029af9658cdca935df60f3bdb3
Aaron Minnick <abminnick@gmail.com>
2022-01-21 14:23:25 -0800

    add models, basic get methods

--- AnimalShelterConsumer/Models/Animal.cs
+++ AnimalShelterConsumer/Models/Animal.cs
@@ -1,7 +1,20 @@
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+
 namespace AnimalShelterConsumer.Models
 {
   public class Animal
   {
-    
+    public int AnimalId {get; set;}
+    public string Name {get; set;}
+    public string Species {get; set;}
+    public string Breed {get; set;}
+
+    public static List<Animal> GetAnimals()
+    {
+      var response = ApiHelper.Get().Result;
+      return JsonConvert.DeserializeObject<List<Animal>>(response);
+    }
   }
 }
--- AnimalShelterConsumer/Models/ApiHelper.cs
+++ AnimalShelterConsumer/Models/ApiHelper.cs
@@ -1,7 +1,16 @@
+using RestSharp;
+using System.Threading.Tasks;
+
 namespace AnimalShelterConsumer.Models
 {
   public static class ApiHelper
   {
-    
+    static RestClient client = new RestClient("http://localhost:5000/api");
+    public static async Task<string> Get()
+    {
+      RestRequest request = new RestRequest($"animals", Method.Get);
+      var response = await client.ExecuteGetAsync(request);
+      return response.Content;
+    }
   }
 }
